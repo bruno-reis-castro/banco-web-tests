7d5ac53b74d8592c79ba138af271f049bc8e7873
bruno-reis-castro <bruno.reis.castro1@gmail.com>
2025-07-21 21:16:20 -0300

    adicionando teste para validar login com credenciais inválidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,4 +7,15 @@ describe('Login', () => {
 
     cy.contains('h4','Realizar Transferência').should('be.visible')
   })
+
+
+  it.only('Login com dados inválidos deve apresentar mensagem de erro', () => {
+    cy.visit('http://localhost:4000/')
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('-')
+    //cy.get('#login-section > .btn').click()
+    cy.contains('button','Entrar').click()
+    cy.get('.toast').should('be.visible').should('have.text','Erro no login. Tente novamente.')
+  })
+
 })
