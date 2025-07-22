e54d82e60c78ce023215eceff39e48dfa97a3c31
bruno-reis-castro <bruno.reis.castro1@gmail.com>
2025-07-22 16:28:10 -0300

    add fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,8 +4,10 @@ describe('Login', () => {
   })
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //cy.visit('http://localhost:4000/')
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.get('#login-section > .btn').click()
 
     cy.contains('h4','Realizar Transferência').should('be.visible')
@@ -14,8 +16,10 @@ describe('Login', () => {
 
   it.only('Login com dados inválidos deve apresentar mensagem de erro', () => {
     //cy.visit('http://localhost:4000/')
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('-')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     //cy.get('#login-section > .btn').click()
     cy.contains('button','Entrar').click()
     cy.get('.toast').should('be.visible').should('have.text','Erro no login. Tente novamente.')
